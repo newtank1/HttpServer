ecfa6c8fb805bdad3159914cf1db04d00074abd2
newtank1 <727786943@qq.com>
2022-06-01 16:05:12 +0800

    fix bug

--- Server/HttpHandler.java
+++ Server/HttpHandler.java
@@ -13,14 +13,10 @@ import java.net.Socket;
 public class HttpHandler implements Runnable{
 
     Socket socket;
-    private final long startTime=System.currentTimeMillis();
     private long endTime;
     private final HttpRequestParser parser=new ParserImpl();
-    private final HttpRequestProcessor processor=new ProcessorImpl();
-    private final HttpResponseBuilder builder=new BuilderImpl();
     private final HttpResponseSender sender=new SenderImpl();
     private final HttpServlet servlet;
-    private Thread daemon;
 
     public HttpHandler(Socket socket) {
         HttpServlet servlet1;
@@ -33,32 +29,32 @@ public class HttpHandler implements Runnable{
             e.printStackTrace();
             servlet1 =new StaticServlet();
         }
+        long startTime = System.currentTimeMillis();
         if(HttpServer.getConfig("Alive")!=null){
             try {
-                endTime=Long.parseLong(HttpServer.getConfig("Alive"))*1000+startTime;
+                endTime=Long.parseLong(HttpServer.getConfig("Alive"))*1000+ startTime;
+                Thread daemon = new Thread(() -> {
+                    while (!socket.isClosed()) {
+                        if (isTimeout()) {
+                            try {
+                                socket.getInputStream().close();
+                                socket.close();
+                                System.out.println("Socket " + socket + " closed in " + endTime + " now " + System.currentTimeMillis());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                daemon.start();
             }catch (NumberFormatException e){
-                endTime=startTime;
+                endTime= startTime;
             }
         }else {
-            endTime=startTime;
+            endTime= startTime;
         }
         servlet = servlet1;
-        System.out.println("Socket "+socket+" init "+startTime+" end in "+endTime);
 
-        daemon=new Thread(()->{
-            while (!socket.isClosed()) {
-                if (isTimeout()) {
-                    try {
-                        socket.getInputStream().close();
-                        socket.close();
-                        System.out.println("Socket "+socket+" closed in "+endTime+" now "+System.currentTimeMillis());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-        daemon.start();
     }
 
     private void process() throws IOException {
@@ -66,7 +62,6 @@ public class HttpHandler implements Runnable{
         HttpResponse response;
         try {
             request=parser.parseRequest(socket);
-            //System.out.println("received request: "+request);
             response=new HttpResponse(request.getVersion());
             servlet.service(request,response);
             if(response.getStatus()==0){
@@ -75,12 +70,10 @@ public class HttpHandler implements Runnable{
         } catch (HttpException e) {
             response= e.buildResponse();
         }
-        //System.out.println("response: "+response);
         long nowTime=System.currentTimeMillis();
         String isAlive = null;
         if (request != null) {
             isAlive=request.getAttribute("Connection");
-            //System.out.println("isAlive: "+isAlive);
         }
         if(response!=null) {
             if("keep-alive".equalsIgnoreCase(isAlive)) {
@@ -89,23 +82,18 @@ public class HttpHandler implements Runnable{
                 } else {
                     response.setAttribute("Connection", "keep-alive");
                     response.setAttribute("Keep-Alive","timeout="+HttpServer.getConfig("Alive")+",max=100");
-                    //System.out.println(response);
                 }
             }
             sender.send(response, socket);
-            System.out.println("Socket "+socket+" response");
         }
         if(!socket.isClosed()&&(nowTime>=endTime||"close".equalsIgnoreCase(isAlive))) {
             socket.close();
-            System.out.println("Socket "+socket+" closed in "+endTime+" now "+nowTime);
         }
-        //System.out.println();
     }
     @Override
     public void run() {
         try {
-            while (System.currentTimeMillis()<=endTime) {
-                //System.out.println("Socket "+socket+" process in "+System.currentTimeMillis());
+            while (!socket.isClosed()) {
                 process();
             }
         } catch (IOException e) {
@@ -117,11 +105,4 @@ public class HttpHandler implements Runnable{
         return System.currentTimeMillis()>=endTime;
     }
 
-    public void close(){
-        try {
-            socket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
